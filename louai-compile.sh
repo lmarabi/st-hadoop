53545242bbd5f821eb517f5f5f41cf9f8d2dab8c
Louai Alarabi <lmarabi@uqu.edu.sa>
2017-02-08 19:54:54 -0600

    Delete louai-compile.sh

--- louai-compile.sh
+++
@@ -1,6 +0,0 @@
-git pull
-mvn compile
-mvn assembly:assembly
-mv ./target/spatialhadoop-2.4.1-SNAPSHOT-uber.jar st-hadoop-uber.jar
-scp st-hadoop-uber.jar louai@cs-spatial-210:/export/scratch/louai/idea-stHadoop/
-cp st-hadoop-uber.jar /export/scratch/louai/scratch1/hadoopDir/localCluster/hadoop-2.7.2/bin/
