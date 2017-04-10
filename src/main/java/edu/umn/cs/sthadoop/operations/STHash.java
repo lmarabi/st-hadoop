4cffe1247a6e72ffea110caa2e3b92342a680e4a
louai Ubuntu <louai@cs.umn.edu>
2017-04-10 15:38:20 -0500

    test and fixing bugs STjoin

---
+++ src/main/java/edu/umn/cs/sthadoop/operations/STHash.java
@@ -0,0 +1,111 @@
+package edu.umn.cs.sthadoop.operations;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
+import edu.umn.cs.spatialHadoop.OperationsParams;
+import edu.umn.cs.sthadoop.core.STPoint;
+
+public class STHash {
+	
+	static class STHashMap extends MapReduceBase implements Mapper<LongWritable, Text, LongWritable, Text> {
+		private double degree = 0.1;
+		private double x1 = -180;
+		private double y1 = -90;
+	
+		
+		@Override
+		public void map(LongWritable key, Text value, OutputCollector<LongWritable, Text> output, Reporter reporter)
+				throws IOException {
+			if(value != null){
+			STPoint shape = new STPoint();
+			shape.fromText(value);
+			// Hasing objects to the grid. 
+			int columnID = (int)((shape.x - x1)/ degree);
+			int rowID = (int)((shape.y - y1)/ degree);
+			output.collect(new LongWritable((columnID*rowID+1)), shape.toText(new Text()));
+			}
+		}
+	}
+
+	static class STHashReduce extends MapReduceBase implements 
+	Reducer<LongWritable, Text, LongWritable, Text> {		
+	
+		@Override
+		public void reduce(final LongWritable cellId, Iterator<Text> values, 
+				final OutputCollector<LongWritable,Text> output,Reporter reporter) throws IOException {
+//			LinkedList<STPoint> shapes = new LinkedList<STPoint>();
+			StringBuilder txt = new StringBuilder();
+			STPoint shape = new STPoint();
+			
+			Text temp = new Text(); 
+			while(values.hasNext()){
+				temp = values.next();
+				txt.append("\t");
+				txt.append(temp.toString());
+			}
+			output.collect(cellId, new Text(txt.toString()));
+			
+		}
+		
+		
+	}
+	
+	/**
+	 * 
+	 * @param inputPath
+	 * @param outputPath
+	 * @param params
+	 * @return
+	 * @throws IOException
+	 * @throws Exception
+	 * @throws InterruptedException
+	 */
+	public static long stHash(Path inputPath, Path outputPath, OperationsParams params)
+			throws IOException, Exception, InterruptedException {
+		
+		JobConf conf = new JobConf(new Configuration(), STHash.class);
+		FileSystem outfs = outputPath.getFileSystem(conf);
+		outfs.delete(outputPath, true);
+		conf.setJobName("STJoin Hashing");
+		// pass params to the join map-reduce 
+		conf.set("timedistance", params.get("timedistance"));
+		conf.set("spacedistance", params.get("spacedistance"));
+		conf.setMapOutputKeyClass(LongWritable.class);
+		conf.setMapOutputValueClass(Text.class);
+		conf.setOutputKeyClass(LongWritable.class);
+		conf.setOutputValueClass(Text.class);
+		// Mapper settings
+		conf.setMapperClass(STHashMap.class);
+		conf.setReducerClass(STHashReduce.class);
+		conf.setCombinerClass(STHashReduce.class);
+		conf.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive", true);
+		conf.setInputFormat(TextInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+		FileInputFormat.setInputPaths(conf, inputPath);
+		FileOutputFormat.setOutputPath(conf, outputPath);
+		conf.setNumReduceTasks(30);
+		JobClient.runJob(conf).waitForCompletion();
+		outfs = inputPath.getFileSystem(conf);
+		outfs.delete(inputPath);
+		return 0;
+	}
+
+}
--- src/main/java/edu/umn/cs/sthadoop/operations/STJoin.java
+++ src/main/java/edu/umn/cs/sthadoop/operations/STJoin.java
@@ -60,50 +60,6 @@ public class STJoin {
 	/** Class logger */
 	private static final Log LOG = LogFactory.getLog(STJoin.class);
 
-	static class STHashMap extends MapReduceBase implements Mapper<LongWritable, Text, LongWritable, Text> {
-		private double degree = 0.1;
-		private double x1 = -180;
-		private double y1 = -90;
-	
-		
-		@Override
-		public void map(LongWritable key, Text value, OutputCollector<LongWritable, Text> output, Reporter reporter)
-				throws IOException {
-			if(value != null){
-			STPoint shape = new STPoint();
-			shape.fromText(value);
-			// Hasing objects to the grid. 
-			int columnID = (int)((shape.x - x1)/ degree);
-			int rowID = (int)((shape.y - y1)/ degree);
-			output.collect(new LongWritable((columnID*rowID+1)), shape.toText(new Text()));
-			}
-		}
-	}
-
-	static class STHashReduce extends MapReduceBase implements 
-	Reducer<LongWritable, Text, LongWritable, Text> {		
-	
-		@Override
-		public void reduce(final LongWritable cellId, Iterator<Text> values, 
-				final OutputCollector<LongWritable,Text> output,Reporter reporter) throws IOException {
-//			LinkedList<STPoint> shapes = new LinkedList<STPoint>();
-			StringBuilder txt = new StringBuilder();
-			STPoint shape = new STPoint();
-			
-			Text temp = new Text(); 
-			while(values.hasNext()){
-				temp = values.next();
-				txt.append("\t");
-				txt.append(temp.toString());
-			}
-			output.collect(cellId, new Text(txt.toString()));
-			
-		}
-		
-		
-	}
-	
-	
 	/**
 	 * The following code is the Join step Refinement. 
 	 */
@@ -206,48 +162,6 @@ public class STJoin {
 		}
 		
 		
-	}
-	
-	
-
-	/**
-	 * 
-	 * @param inputPath
-	 * @param outputPath
-	 * @param params
-	 * @return
-	 * @throws IOException
-	 * @throws Exception
-	 * @throws InterruptedException
-	 */
-	private static long stHash(Path inputPath, Path outputPath, OperationsParams params)
-			throws IOException, Exception, InterruptedException {
-		
-		JobConf conf = new JobConf(new Configuration(), STJoin.class);
-		FileSystem outfs = outputPath.getFileSystem(conf);
-		outfs.delete(outputPath, true);
-		conf.setJobName("STJoin Hashing");
-		// pass params to the join map-reduce 
-		conf.set("timedistance", params.get("timedistance"));
-		conf.set("spacedistance", params.get("spacedistance"));
-		conf.setMapOutputKeyClass(LongWritable.class);
-		conf.setMapOutputValueClass(Text.class);
-		conf.setOutputKeyClass(LongWritable.class);
-		conf.setOutputValueClass(Text.class);
-		// Mapper settings
-		conf.setMapperClass(STHashMap.class);
-		conf.setReducerClass(STHashReduce.class);
-		conf.setCombinerClass(STHashReduce.class);
-		conf.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive", true);
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
-		FileInputFormat.setInputPaths(conf, inputPath);
-		FileOutputFormat.setOutputPath(conf, outputPath);
-		conf.setNumReduceTasks(30);
-		JobClient.runJob(conf).waitForCompletion();
-		outfs = inputPath.getFileSystem(conf);
-		outfs.delete(inputPath);
-		return 0;
 	}
 	
 	
@@ -402,16 +316,16 @@ public class STJoin {
 	    Path hashedbucket = new Path(outputPath.getParent().toString()+"/hashedbucket");
 		long t1 = System.currentTimeMillis();
 		// join hash step 
-		long resultSize = stHash(inputstjoin, hashedbucket, params);
+		long resultSize = STHash.stHash(inputstjoin, hashedbucket, params);
 		
-		//join Step
-		if(fs.exists(new Path(outputPath.getParent().toString()+"hashedbucket"))){
-	    	inputstjoin = new Path(outputPath.getParent().toString()+"hashedbucket");
-	    }else{
-	    	inputstjoin = new Path(outputPath.getParent().toString()+"/hashedbucket");
-	    }
+//		//join Step
+//		if(fs.exists(new Path(outputPath.getParent().toString()+"hashedbucket"))){
+//	    	inputstjoin = new Path(outputPath.getParent().toString()+"hashedbucket");
+//	    }else{
+//	    	inputstjoin = new Path(outputPath.getParent().toString()+"/hashedbucket");
+//	    }
 		//Join refinement Step 
-		stJoin(inputstjoin, hashedbucket, params);
+		stJoin(hashedbucket, outputPath, params);
 		long t2 = System.currentTimeMillis();
 		System.out.println("Total time: " + (t2 - t1) + " millis");
 	}
