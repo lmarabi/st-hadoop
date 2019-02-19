644402e758b79a66c5ff69783dd15a8781c63ec2
louai Alarabi <lmarabi@uqu.edu.sa>
2019-02-19 14:29:22 -0600

    update similarity function

--- src/main/java/edu/umn/cs/sthadoop/trajectory/KNNTrajectory.java
+++ src/main/java/edu/umn/cs/sthadoop/trajectory/KNNTrajectory.java
@@ -148,6 +148,7 @@ public class KNNTrajectory {
 		}
 		return list;
 	}
+	
 
 	private static void printUsage() {
 		System.out.println("Performs a KNN DTW query on an input file");
@@ -170,19 +171,16 @@ public class KNNTrajectory {
 	public static void main(String[] args) throws Exception {
 
 		// with specifying the level
-		// args = new String[9];
-		// args[0] =
-		// "/export/scratch/mntgData/geolifeGPS/geolife_Trajectories_1.3/HDFS/index_geolife";
-		// args[1] =
-		// "/export/scratch/mntgData/geolifeGPS/geolife_Trajectories_1.3/HDFS/knn-dis-result";
-		// args[2] = "shape:edu.umn.cs.sthadoop.trajectory.GeolifeTrajectory";
-		// args[3] = "interval:2008-05-01,2008-05-30";
-		// args[4] = "time:month";
-		// args[5] = "k:100";
-		// args[6] =
-		// "traj:39.9119983,116.606835x39.9119783,116.6065483x39.9119599,116.6062649x39.9119416,116.6059899x39.9119233,116.6057282x39.9118999,116.6054783x39.9118849,116.6052366x39.9118666,116.6050099x39.91185,116.604775x39.9118299,116.604525x39.9118049,116.6042649x39.91177,116.6040166x39.9117516,116.6037583x39.9117349,116.6035066x39.9117199,116.6032666x39.9117083,116.6030232x39.9117,116.6027566x39.91128,116.5969383x39.9112583,116.5966766x39.9112383,116.5964232x39.9112149,116.5961699x39.9111933,116.5959249x39.9111716,116.5956883";
-		// args[7] = "-overwrite";
-		// args[8] = "-no-local";// "-local";
+//		args = new String[9];
+//		args[0] = "/export/scratch/mntgData/summit/dataset/str";
+//		args[1] = "/export/scratch/mntgData/summit/dataset/result/knn-dis-result";
+//		args[2] = "shape:edu.umn.cs.sthadoop.core.STNycSTPoints";
+//		args[3] = "interval:2014-10-01,2014-10-01";
+//		args[4] = "time:day";
+//		args[5] = "k:100";
+//		args[6] = "traj:-73.9972915649414,40.682044982910156x-73.997872,40.682957x-73.997551,40.683629x-73.99722599999998,40.684298x-73.99635850000001,40.6860951x-73.99604099999999,40.686752999999996x-73.995732,40.687397000000004x-73.995497,40.687884999999994x-73.995333,40.688226x-73.995001,40.688915x-73.994678,40.689583x-73.9943596,40.69024279999999x-73.994046,40.69089700000001x-73.99373600000001,40.691534999999995x-73.993438,40.692153999999995x-73.99310299999999,40.692846x-73.992744,40.693605x-73.9924155,40.6942755x-73.99210000000001,40.694947x-73.99156690000001,40.6961229x-73.9914998,40.6961646x-73.9913637,40.696187900000005x-73.99116620000001,40.6961937x-73.99004120000001,40.696146600000006x-73.98896040000002,40.69610659999999x-73.98885870000001,40.6961025x-73.9886937,40.69609799999999x-73.98868159999998,40.696299499999995x-73.9886783,40.6963508x-73.98853940000001,40.6985153x-73.9884958,40.699194999999996x-73.98850279999999,40.6993448x-73.98853709999999,40.69946839999999x-73.9885972,40.6995985x-73.98867440000001,40.69972220000001x-73.9887689,40.699839299999994x-73.9888804,40.6999434x-73.9890264,40.7000605x-73.98934849999999,40.70025920000001x-73.9895549,40.7003964x-73.9897835,40.7005765x-73.990101,40.700835x-73.9903534,40.701042799999996x-74.0018019,40.7101218x-74.00217540000001,40.71041989999999x-74.0024481,40.7106376x-74.0036669,40.711607x-74.00414320000002,40.71196679999999x-74.0043619,40.712142899999996x-74.00442319999999,40.712206200000004x-74.0044588,40.712255299999995x-74.0044942,40.71232450000001x-74.0045008,40.712368600000005x-74.00450859999998,40.7124146x-74.00450409999999,40.712497299999995x-74.0044765,40.71260590000001x-74.0044099,40.7127226x-74.00437769999999,40.71282119999999x-74.0046331,40.7126318x-74.00485059999998,40.7124788x-74.0049167,40.7124347x-74.0052547,40.7122771x-74.0056323,40.7121466x-74.0057898,40.71209029999999x-74.0059794,40.712036700000006x-74.00621570000001,40.711960499999996x-74.00692569999998,40.7117603x-74.0070243,40.711735600000004x-74.00711779999999,40.7117118x-74.00733190000001,40.7117116x-74.0075469,40.71166989999999x-74.00767390000001,40.7116745x-74.0077635,40.7116988x-74.007823,40.7117257x-74.0078875,40.7117675x-74.00799730000001,40.7118889x-74.0081432,40.7119995x-74.00865820000001,40.7113879x-74.00845790000001,40.711247799999995x-74.0073597,40.710587700000005x-74.0073263,40.710550299999994x-74.00607600000001,40.70991779999999x-74.0060473,40.7099466";
+//		args[7] = "-overwrite";
+//		args[8] = "-no-local";// "-local";
 
 		// without specifying the level
 		// args = new String[8];
@@ -197,6 +195,21 @@ public class KNNTrajectory {
 		// "traj:39.9119983,116.606835x39.9119783,116.6065483x39.9119599,116.6062649x39.9119416,116.6059899x39.9119233,116.6057282x39.9118999,116.6054783x39.9118849,116.6052366x39.9118666,116.6050099x39.91185,116.604775x39.9118299,116.604525x39.9118049,116.6042649x39.91177,116.6040166x39.9117516,116.6037583x39.9117349,116.6035066x39.9117199,116.6032666x39.9117083,116.6030232x39.9117,116.6027566x39.91128,116.5969383x39.9112583,116.5966766x39.9112383,116.5964232x39.9112149,116.5961699x39.9111933,116.5959249x39.9111716,116.5956883";
 		// args[6] = "-overwrite";
 		// args[7] = "-no-local";// "-local";
+
+		// with segmentation based.
+		 args = new String[9];
+		 args[0] =
+		 "/export/scratch/mntgData/summit/dataset/str+";
+		 args[1] =
+		 "/export/scratch/mntgDa	ta/summit/dataset/result/knn-dis-result";
+		 args[2] = "shape:edu.umn.cs.sthadoop.core.STNycTrajectory";
+		 args[3] = "interval:2014-10-01,2014-10-01";
+		 args[4] = "time:day";
+		 args[5] = "k:100";
+		 args[6] =
+		 "traj:-73.9972915649414,40.682044982910156x-73.997872,40.682957x-73.997551,40.683629x-73.99722599999998,40.684298x-73.99635850000001,40.6860951x-73.99604099999999,40.686752999999996x-73.995732,40.687397000000004x-73.995497,40.687884999999994x-73.995333,40.688226x-73.995001,40.688915x-73.994678,40.689583x-73.9943596,40.69024279999999x-73.994046,40.69089700000001x-73.99373600000001,40.691534999999995x-73.993438,40.692153999999995x-73.99310299999999,40.692846x-73.992744,40.693605x-73.9924155,40.6942755x-73.99210000000001,40.694947x-73.99156690000001,40.6961229x-73.9914998,40.6961646x-73.9913637,40.696187900000005x-73.99116620000001,40.6961937x-73.99004120000001,40.696146600000006x-73.98896040000002,40.69610659999999x-73.98885870000001,40.6961025x-73.9886937,40.69609799999999x-73.98868159999998,40.696299499999995x-73.9886783,40.6963508x-73.98853940000001,40.6985153x-73.9884958,40.699194999999996x-73.98850279999999,40.6993448x-73.98853709999999,40.69946839999999x-73.9885972,40.6995985x-73.98867440000001,40.69972220000001x-73.9887689,40.699839299999994x-73.9888804,40.6999434x-73.9890264,40.7000605x-73.98934849999999,40.70025920000001x-73.9895549,40.7003964x-73.9897835,40.7005765x-73.990101,40.700835x-73.9903534,40.701042799999996x-74.0018019,40.7101218x-74.00217540000001,40.71041989999999x-74.0024481,40.7106376x-74.0036669,40.711607x-74.00414320000002,40.71196679999999x-74.0043619,40.712142899999996x-74.00442319999999,40.712206200000004x-74.0044588,40.712255299999995x-74.0044942,40.71232450000001x-74.0045008,40.712368600000005x-74.00450859999998,40.7124146x-74.00450409999999,40.712497299999995x-74.0044765,40.71260590000001x-74.0044099,40.7127226x-74.00437769999999,40.71282119999999x-74.0046331,40.7126318x-74.00485059999998,40.7124788x-74.0049167,40.7124347x-74.0052547,40.7122771x-74.0056323,40.7121466x-74.0057898,40.71209029999999x-74.0059794,40.712036700000006x-74.00621570000001,40.711960499999996x-74.00692569999998,40.7117603x-74.0070243,40.711735600000004x-74.00711779999999,40.7117118x-74.00733190000001,40.7117116x-74.0075469,40.71166989999999x-74.00767390000001,40.7116745x-74.0077635,40.7116988x-74.007823,40.7117257x-74.0078875,40.7117675x-74.00799730000001,40.7118889x-74.0081432,40.7119995x-74.00865820000001,40.7113879x-74.00845790000001,40.711247799999995x-74.0073597,40.710587700000005x-74.0073263,40.710550299999994x-74.00607600000001,40.70991779999999x-74.0060473,40.7099466";
+		 args[7] = "-overwrite";
+		 args[8] = "-no-local";// "-local";
 
 		String inputPath, intermediatePath, outputPath;
 		int topk = 0;
@@ -254,13 +267,23 @@ public class KNNTrajectory {
 
 		PriorityQueue<KNNTrajectory.Score> pqueue = new PriorityQueue<KNNTrajectory.Score>();
 		HashMap<String, List<STPoint>> overlappedTrajectory = new HashMap<String, List<STPoint>>();
-		HashMap<String, List<STNycTrajectory>> overlappedTrajectory_rect = new HashMap<String, List<STNycTrajectory>>();
+		HashMap<String, STNycTrajectory> overlappedTrajectory_rect = new HashMap<String, STNycTrajectory>();
 		Configuration conf = new Configuration();
 		Job job = Job.getInstance(conf);
 		FileSystem fs = FileSystem.get(conf);
 		Path checkinterpath = new Path(intermediatePath);
 		if (fs.exists(checkinterpath)) {
 			fs.delete(checkinterpath);
+			fs.create(checkinterpath);
+		} else {
+			fs.create(checkinterpath);
+		}
+		checkinterpath = new Path(outputPath);
+		if (!fs.exists(checkinterpath)) {
+			fs.create(checkinterpath);
+		} else {
+			fs.delete(checkinterpath);
+			fs.create(checkinterpath);
 		}
 		int numTry = 3;
 		long t1 = System.currentTimeMillis();
@@ -310,19 +333,15 @@ public class KNNTrajectory {
 								plist.add((STPoint) temp);
 								overlappedTrajectory.put(temp.id, plist);
 							}
-						}
-						else if((inObj instanceof STRectangle)){
+						} else if ((inObj instanceof STRectangle)) {
 							STNycTrajectory temp = new STNycTrajectory(line);
 							if (overlappedTrajectory.containsKey(temp.id)) {
-								List<STNycTrajectory> plist = overlappedTrajectory_rect
+								STNycTrajectory plist = overlappedTrajectory_rect
 										.get(temp.id);
-								plist.add((STNycTrajectory) temp);
 								overlappedTrajectory_rect.put(temp.id, plist);
 							} else {
-								// new trajectory inserted.
-								List<STNycTrajectory> plist = new ArrayList<STNycTrajectory>();
-								plist.add((STNycTrajectory) temp);
-								overlappedTrajectory_rect.put(temp.id, plist);
+								
+								overlappedTrajectory_rect.put(temp.id, temp);
 							}
 						}
 
@@ -336,15 +355,25 @@ public class KNNTrajectory {
 			fs.delete(new Path(intermediatePath));
 			// find the top k from the intermediate resutl.
 			List<STPoint> query = getTrajectoryQueryPoints(params.get("traj"));
-
-			Iterator it = overlappedTrajectory.entrySet().iterator();
-			while (it.hasNext()) {
-				Map.Entry entry = (Map.Entry) it.next();
-				String id = (String) entry.getKey();
-				List<STPoint> traj = (List<STPoint>) entry.getValue();
-				Collections.sort(traj);
-				DTW similarityfunc = new DTW(query, traj);
-				pqueue.add(new Score(id, similarityfunc.getDistance()));
+			if ((inObj instanceof STPoint)) {
+				Iterator it = overlappedTrajectory.entrySet().iterator();
+				while (it.hasNext()) {
+					Map.Entry entry = (Map.Entry) it.next();
+					String id = (String) entry.getKey();
+					List<STPoint> traj = (List<STPoint>) entry.getValue();
+					Collections.sort(traj);
+					DTW similarityfunc = new DTW(query, traj);
+					pqueue.add(new Score(id, similarityfunc.getDistance()));
+				}
+			}else if((inObj instanceof STRectangle)) {
+				Iterator it = overlappedTrajectory_rect.entrySet().iterator();
+				while (it.hasNext()) {
+					Map.Entry entry = (Map.Entry) it.next();
+					String id = (String) entry.getKey();
+					STNycTrajectory traj_rect = (STNycTrajectory) entry.getValue();
+					DTW similarityfunc = new DTW(query, traj_rect.getPointsList());
+					pqueue.add(new Score(id, similarityfunc.getDistance()));
+				}
 			}
 
 			numTry--;
@@ -367,13 +396,20 @@ public class KNNTrajectory {
 				Score temp = pqueue.poll();
 				System.out.println(temp.id + "\tSimilarity Score: "
 						+ temp.score);
-				List<STPoint> trajectory = overlappedTrajectory.get(temp.id);
-				Collections.sort(trajectory);
-				for (STPoint p : trajectory) {
-					String text = p.x + "," + p.y + ";";
-					text.replace("\00", "");
-					fout.writeBytes(text);
+				if ((inObj instanceof STPoint)) {
+					List<STPoint> trajectory = overlappedTrajectory.get(temp.id);
+					Collections.sort(trajectory);
+					for (STPoint p : trajectory) {
+						String text = p.x + "," + p.y + ";";
+						text.replace("\00", "");
+						fout.writeBytes(text);
 
+					}
+				}else if((inObj instanceof STRectangle)) {
+					STNycTrajectory trajectory = overlappedTrajectory_rect.get(temp.id);
+						String text = trajectory.toString();
+						text.replace("\00", "");
+						fout.writeBytes(text);
 				}
 				fout.writeBytes("\n");
 			}
